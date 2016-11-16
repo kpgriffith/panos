c9e3ca462b87158e5de014bb707be9329a132c1f
Kevin Griffith <kpgriffith@me.com>
2016-11-16 10:12:51 -0600

    adding rspec for address request scenarios and updating gitignore

--- lib/panos/panorama/requests/address_request.rb
+++ lib/panos/panorama/requests/address_request.rb
@@ -1,36 +1,36 @@
 require 'panos/models/key'
 require 'panos/panorama/models/address'
+require 'panos'
 
 # This class provides functions for updating the address object on a PANOS firewall device
 module Panos
   module Panorama
     class AddressRequest
 
-      # TODO need to add logic for the different types of address.
       # Update will update the address in the firewall device
       # right now it just updates the IP address
       def update(address)
-        fail ArgumentError, 'address must be of type Address' if !address.is_a? Address
+        fail ArgumentError, 'address must be of type Address' unless address.is_a? Address
 
-        if address.type =~ /Ip_Netmask/
+        if 'IP_Netmask'.casecmp(address.type) == 0
           xpath = "/config/devices/entry[@name='localhost.localdomain']/device-group/entry[@name='#{address.device_group}']/address/entry[@name='#{address.name}']/ip-netmask"
           element = "<ip-netmask>#{address.address}</ip-netmask>"
-        elsif address.type =~ /Ip_Range/
-          xpath = ''
-          element = ''
+        elsif 'IP_Range'.casecmp(address.type) == 0
+          xpath = "/config/devices/entry[@name='localhost.localdomain']/device-group/entry[@name='#{address.device_group}']/address/entry[@name='#{address.name}']/ip-range"
+          element = "<ip-range>#{address.address}</ip-range>"
         else   # fqdn scenario
-          xpath = ''
-          element = ''
+          xpath = "/config/devices/entry[@name='localhost.localdomain']/device-group/entry[@name='#{address.device_group}']/address/entry[@name='#{address.name}']/fqdn"
+          element = "<fqdn>#{address.address}</fqdn>"
         end
 
         begin
         	set_address_response = RestClient::Request.execute(
         		:method => :post,
         		:verify_ssl => false,
-        		:url => @baseurl,
+        		:url => $baseurl,
         		:headers => {
         			:params => {
-        				:key => @key.value,
+        				:key => $key.value,
         				:type => 'config',
         				:action => 'edit',
         				:xpath => xpath,
@@ -48,16 +48,16 @@ module Panos
 
       # get returns the Address from the name of the address given.
       def get(address)
-        fail ArgumentError, 'address must be of type Address' if !address.is_a? Address
+        fail ArgumentError, 'address must be of type Address' unless address.is_a? Address
 
         begin
         	get_response = RestClient::Request.execute(
         		:method => :get,
         		:verify_ssl => false,
-        		:url => @baseurl,
+        		:url => $baseurl,
         		:headers => {
         			:params => {
-        				:key => @key.value,
+        				:key => $key.value,
         				:type => 'config',
         				:action => 'get',
         				:xpath => "/config/devices/entry[@name='localhost.localdomain']/device-group/entry[@name='#{address.device_group}']/address/entry[@name='#{address.name}']"
@@ -68,14 +68,25 @@ module Panos
           raise Exception.new("PANOS error getting address: #{get_hash['response']['msg']}") if get_hash['response']['status'] == 'error'
           # if the entry isn't found, the response from PANOS isn't an error, the result attribute
           # in the XML response is nil
-          if !get_hash['response']['result'].nil?
+          if get_hash['response'].has_key?('result')
             entry = get_hash['response']['result']['entry']
-            # tag is optional so we have to check for nil
-            tag = !entry['tag'].nil? ? nil : entry['tag']['member']
 
-            # TODO add logic here to look at the entry key to know what type of address it is.
-            @address = Address.new(entry['name'], 'IP_NETMASK', entry['ip_netmask'], tag)
-            return @address
+            # tag is optional so we have to check for nil
+            tag = !entry.has_key?('tag') ? nil : entry['tag']['member']
+
+            if entry.has_key?('ip_netmask')
+              @address = Address.new(entry['name'], 'IP_NETMASK', entry['ip_netmask'], entry['loc'], tag)
+              return @address
+            elsif entry.has_key?('ip_range')
+              @address = Address.new(entry['name'], 'IP_RANGE', entry['ip_range'], entry['loc'], tag)
+              return @address
+            elsif entry.has_key?('fqdn')
+              @address = Address.new(entry['name'], 'FQDN', entry['fqdn'], entry['loc'], tag)
+              return @address
+            else
+              #must be a new type not yet implemented.
+              return nil
+            end
           else
             return nil
           end
@@ -90,19 +101,29 @@ module Panos
         return bool
       end
 
-      def create(name, ip, tag, device_group)
+      def create(address)
+        fail ArgumentError, 'address must be of type Address' unless address.is_a? Address
+
+        if 'IP_Netmask'.casecmp(address.type) == 0
+          element = "<entry name='#{address.name}'><ip-netmask>#{address.address}</ip-netmask><tag><member>#{address.tags}</member></tag></entry>"
+        elsif 'IP_Range'.casecmp(address.type) == 0
+          element = "<entry name='#{address.name}'><ip-range>#{address.address}</ip-range><tag><member>#{address.tags}</member></tag></entry>"
+        else   # fqdn scenario
+          element = "<entry name='#{address.name}'><fqdn>#{address.address}</fqdn><tag><member>#{address.tags}</member></tag></entry>"
+        end
+
         begin
         	set_address_response = RestClient::Request.execute(
         		:method => :post,
         		:verify_ssl => false,
-        		:url => @baseurl,
+        		:url => $baseurl,
         		:headers => {
         			:params => {
-        				:key => @key.value,
+        				:key => $key.value,
         				:type => 'config',
         				:action => 'set',
-        				:xpath => "/config/devices/entry[@name='localhost.localdomain']/device-group/entry[@name='#{device_group}']/address",
-        				:element => "<entry name='#{name}'><ip-netmask>#{ip}</ip-netmask><tag><member>#{tag}</member></tag></entry>"
+        				:xpath => "/config/devices/entry[@name='localhost.localdomain']/device-group/entry[@name='#{address.device_group}']/address",
+        				:element => element
         			}
         		}
         	)
@@ -114,16 +135,16 @@ module Panos
       end
 
       def delete(address)
-        fail ArgumentError, 'address must be of type Address' if !address.is_a? Address
+        fail ArgumentError, 'address must be of type Address' unless address.is_a? Address
 
         begin
         	delete_address_response = RestClient::Request.execute(
         		:method => :post,
         		:verify_ssl => false,
-        		:url => @baseurl,
+        		:url => $baseurl,
         		:headers => {
         			:params => {
-        				:key => @key.value,
+        				:key => $key.value,
         				:type => 'config',
         				:action => 'delete',
                 :xpath => "/config/devices/entry[@name='localhost.localdomain']/device-group/entry[@name='#{address.device_group}']/address/entry[@name='#{address.name}']"
@@ -143,10 +164,10 @@ module Panos
           get_response = RestClient::Request.execute(
             :method => :get,
             :verify_ssl => false,
-            :url => @baseurl,
+            :url => $baseurl,
             :headers => {
               :params => {
-                :key => @key.value,
+                :key => $key.value,
                 :type => 'config',
                 :action => 'get',
                 :xpath => "/config/devices/entry[@name='localhost.localdomain']/device-group/entry[@name='#{device_group}']/address"
@@ -157,7 +178,7 @@ module Panos
           raise Exception.new("PANOS error getting address: #{get_all_hash['response']['msg']}") if get_all_hash['response']['status'] == 'error'
           # if the entry isn't found, the response from PANOS isn't an error, the result attribute
           # in the XML response is nil
-          if !get_all_hash['response']['result'].nil?
+          if get_all_hash['response'].has_key?('result') && !get_all_hash['response']['result'].nil?
             entries = get_all_hash['response']['result']['address']['entry']
             entries.each do |entry|
               # tag is optional so we have to check for nil
@@ -167,7 +188,13 @@ module Panos
               end
               # add the address to an array if the tags are equal
               if !tag.nil? && tag == tag_name
-                address_array.push(Address.new(entry['name'], 'IP_NETMASK', entry['ip_netmask'], tag))
+                if entry.has_key?('ip_netmask')
+                  address_array.push(Address.new(entry['name'], 'IP_NETMASK', entry['ip_netmask'], entry['loc'], tag))
+                elsif entry.has_key?('ip_range')
+                  address_array.push(Address.new(entry['name'], 'IP_RANGE', entry['ip_range'], entry['loc'], tag))
+                elsif entry.has_key?('fqdn')
+                  address_array.push(Address.new(entry['name'], 'FQDN', entry['fqdn'], entry['loc'], tag))
+                end
               end
             end
           end
--- spec/panos/panorama/requests/address_request_spec.rb
+++ spec/panos/panorama/requests/address_request_spec.rb
@@ -1,54 +1,230 @@
 require 'spec_helper'
 require 'panos/panorama/requests/address_request'
 require 'panos/models/key'
+require 'panos/panorama/models/address'
+require 'panos'
 
+include Panos
 include Panos::Panorama
 
 describe 'Address Request' do
 
-  context 'get address' do
-    it 'returns a valid address object if found'
-
-    it 'returns a nil if no address was found'
-
-    it 'throws an exception when there was an error from the firewall'
-
-  end
-
-  context 'address exists' do
-    it 'returns true when the address is not nil'
-
-    it 'returns false when the address is nil'
-
+  before do
+    @addrequest = AddressRequest.new
+    # must create the global variables needed to
+    $key = Key.new('key')
+    $baseurl = 'url'
   end
 
   context 'update address' do
-    it 'fails if the arguement is not an Address type'
+    it 'fails if the arguement is not an Address type' do
+      expect{@addrequest.update('group-request')}.to raise_error(ArgumentError)
+    end
 
-    it 'throws an exception when there was an error from the firewall'
+    it 'throws an exception when there was an error from the firewall' do
+      addr = Address.new('test', 'IP_NETMASK', '1.1.1.1', 'group')
+      response = "<response status = 'error' code = '403'><result><msg>Invalid credentials.</msg></result></response>"
+      allow(RestClient::Request).to receive(:execute).and_return(response)
+      expect{@addrequest.update(addr)}.to raise_error(Exception)
+    end
+
+    it 'throws an exception making the rest call' do
+      addr = Address.new('test', 'IP_NETMASK', '1.1.1.1', 'group')
+      allow(RestClient::Request).to receive(:execute).and_raise(Exception)
+      expect{@addrequest.update(addr)}.to raise_error(Exception)
+    end
+
+    it 'succeeds when it updates the address for ip_netmask' do
+      addr = Address.new('test', 'IP_NETMASK', '1.1.1.1', 'group')
+      response = "<response status = 'success'></response>"
+      allow(RestClient::Request).to receive(:execute).and_return(response)
+      expect{@addrequest.update(addr)}.to_not raise_error
+    end
+
+    it 'succeeds when it updates the address for ip_range' do
+      addr = Address.new('test', 'IP_RANGE', '1.1.1.1', 'group')
+      response = "<response status = 'success'></response>"
+      allow(RestClient::Request).to receive(:execute).and_return(response)
+      expect{@addrequest.update(addr)}.to_not raise_error
+    end
+
+    it 'succeeds when it updates the address for fqdn' do
+      addr = Address.new('test', 'FQDN', 'hello.myname.is', 'group')
+      response = "<response status = 'success'></response>"
+      allow(RestClient::Request).to receive(:execute).and_return(response)
+      expect{@addrequest.update(addr)}.to_not raise_error
+    end
 
   end
 
-  context 'create address' do
-    it 'succeeds when it creates the address'
+  context 'get address' do
+    it 'fails if the arguement is not an Address type' do
+      expect{@addrequest.get('group-request')}.to raise_error(ArgumentError)
+    end
 
-    it 'throws an exception when there was an error from the firewall'
+    it 'returns a valid address object for ip_netmask with a tag if found' do
+      addr = Address.new('test', 'IP_NETMASK', '1.1.1.1', 'group')
+      response = "<response status='success'><result><entry name='name' loc='group'><ip-netmask>1.1.1.1</ip-netmask><tag><member>tag</member></tag></entry></result></response>"
+      allow(RestClient::Request).to receive(:execute).and_return(response)
+      expect{@addrequest.get(addr)}.to_not raise_error
+    end
+
+    it 'returns a valid address object for ip_netmask without a tag if found' do
+      addr = Address.new('test', 'IP_NETMASK', '1.1.1.1', 'group')
+      response = "<response status='success'><result><entry name='name' loc='group'><ip-netmask>1.1.1.1</ip-netmask></entry></result></response>"
+      allow(RestClient::Request).to receive(:execute).and_return(response)
+      expect{@addrequest.get(addr)}.to_not raise_error
+    end
+
+    it 'returns a valid address object for ip_range with a tag if found' do
+      addr = Address.new('test', 'IP_RANGE', '1.1.1.0-1.1.1.9', 'group')
+      response = "<response status='success'><result><entry name='name' loc='group'><ip-range>1.1.1.0-1.1.1.9</ip-range><tag><member>tag</member></tag></entry></result></response>"
+      allow(RestClient::Request).to receive(:execute).and_return(response)
+      expect{@addrequest.get(addr)}.to_not raise_error
+    end
+
+    it 'returns a valid address object for ip_range without a tag if found' do
+      addr = Address.new('test', 'IP_RANGE', '1.1.1.0-1.1.1.9', 'group')
+      response = "<response status='success'><result><entry name='name' loc='group'><ip-range>1.1.1.0-1.1.1.9</ip-range></entry></result></response>"
+      allow(RestClient::Request).to receive(:execute).and_return(response)
+      expect{@addrequest.get(addr)}.to_not raise_error
+    end
+
+    it 'returns a valid address object for fqdn with a tag if found' do
+      addr = Address.new('test', 'FQDN', '1.1.1.1', 'group')
+      response = "<response status='success'><result><entry name='name' loc='group'><fqdn>test.com</fqdn><tag><member>tag</member></tag></entry></result></response>"
+      allow(RestClient::Request).to receive(:execute).and_return(response)
+      expect{@addrequest.get(addr)}.to_not raise_error
+    end
+
+    it 'returns a valid address object for fqdn without a tag if found' do
+      addr = Address.new('test', 'FQDN', '1.1.1.1', 'group')
+      response = "<response status='success'><result><entry name='name' loc='group'><fqdn>test.com</fqdn></entry></result></response>"
+      allow(RestClient::Request).to receive(:execute).and_return(response)
+      expect{@addrequest.get(addr)}.to_not raise_error
+    end
+
+    it 'returns a nil if no address was found' do
+      addr = Address.new('test', 'IP_NETMASK', '1.1.1.1', 'group')
+      response = "<response status='success'></response>"
+      allow(RestClient::Request).to receive(:execute).and_return(response)
+      expect{@addrequest.get(addr)}.to be_a NilClass
+    end
+
+    it 'throws an exception when there was an error from the firewall' do
+      addr = Address.new('test', 'IP_NETMASK', '1.1.1.1', 'group')
+      response = "<response status = 'error' code = '403'><result><msg>Invalid credentials.</msg></result></response>"
+      allow(RestClient::Request).to receive(:execute).and_return(response)
+      expect{@addrequest.get(addr)}.to raise_error(Exception)
+    end
+
+    it 'throws an exception making the rest call' do
+      addr = Address.new('test', 'IP_NETMASK', '1.1.1.1', 'group')
+      allow(RestClient::Request).to receive(:execute).and_raise(Exception)
+      expect{@addrequest.get(addr)}.to raise_error(Exception)
+    end
+  end
+
+  context 'create address' do
+    it 'succeeds when it creates the address for ip_netmask' do
+      addr = Address.new('test', 'IP_NETMASK', '1.1.1.1', 'group')
+      response = "<response status = 'success'></response>"
+      allow(RestClient::Request).to receive(:execute).and_return(response)
+      expect{@addrequest.create(addr)}.to_not raise_error
+    end
+
+    it 'succeeds when it creates the address for ip_range' do
+      addr = Address.new('test', 'IP_RANGE', '1.1.1.1', 'group')
+      response = "<response status = 'success'></response>"
+      allow(RestClient::Request).to receive(:execute).and_return(response)
+      expect{@addrequest.create(addr)}.to_not raise_error
+    end
+
+    it 'succeeds when it creates the address for fqdn' do
+      addr = Address.new('test', 'FQDN', 'hello.myname.is', 'group')
+      response = "<response status = 'success'></response>"
+      allow(RestClient::Request).to receive(:execute).and_return(response)
+      expect{@addrequest.create(addr)}.to_not raise_error
+    end
+
+    it 'throws an exception when there was an error from the firewall' do
+      addr = Address.new('test', 'IP_NETMASK', '1.1.1.1', 'group')
+      response = "<response status = 'error' code = '403'><result><msg>Invalid credentials.</msg></result></response>"
+      allow(RestClient::Request).to receive(:execute).and_return(response)
+      expect{@addrequest.create(addr)}.to raise_error(Exception)
+    end
+
+    it 'fails if the arguement is not an Address type' do
+      expect{@addrequest.create('group-request')}.to raise_error(ArgumentError)
+    end
+
+    it 'throws an exception making the rest call' do
+      addr = Address.new('test', 'IP_NETMASK', '1.1.1.1', 'group')
+      allow(RestClient::Request).to receive(:execute).and_raise(Exception)
+      expect{@addrequest.create(addr)}.to raise_error(Exception)
+    end
 
   end
 
   context 'delete address' do
-    it 'succeeds when it delets the address'
+    it 'succeeds when it deletes the address' do
+      addr = Address.new('test', 'IP_NETMASK', '1.1.1.1', 'group')
+      response = "<response status = 'success'></response>"
+      allow(RestClient::Request).to receive(:execute).and_return(response)
+      expect{@addrequest.delete(addr)}.to_not raise_error
+    end
 
-    it 'throws an exception when there was an error from the firewall'
+    it 'throws an exception when there was an error from the firewall' do
+      addr = Address.new('test', 'IP_NETMASK', '1.1.1.1', 'group')
+      response = "<response status = 'error' code = '403'><result><msg>Invalid credentials.</msg></result></response>"
+      allow(RestClient::Request).to receive(:execute).and_return(response)
+      expect{@addrequest.delete(addr)}.to raise_error(Exception)
+    end
+
+    it 'fails if the arguement is not an Address type' do
+      expect{@addrequest.delete('group-request')}.to raise_error(ArgumentError)
+    end
+
+    it 'throws an exception making the rest call' do
+      addr = Address.new('test', 'IP_NETMASK', '1.1.1.1', 'group')
+      allow(RestClient::Request).to receive(:execute).and_raise(Exception)
+      expect{@addrequest.delete(addr)}.to raise_error(Exception)
+    end
 
   end
 
   context 'get all addresses with tag name' do
-    it 'returns all the addresses with the tag name'
+    it 'returns all the addresses with the tag name, expect 1 to return' do
+      response = "<response status='success'><result><address><entry name='name' loc='group'><ip-netmask>1.1.1.1</ip-netmask><tag><member>tag</member></tag></entry><entry name='name2' loc='group'><ip-netmask>1.1.1.2</ip-netmask><tag><member>tag2</member></tag></entry></address></result></response>"
+      allow(RestClient::Request).to receive(:execute).and_return(response)
+      tag_array = @addrequest.get_all_for_tag('tag', 'group')
+      expect{tag_array.length == 1}
+    end
 
-    it 'returns nil for no addresses found with the tag name'
+    it 'returns empty array for no addresses found with the tag name' do
+      response = "<response status='success'><result><address><entry name='name' loc='group'><ip-netmask>1.1.1.1</ip-netmask><tag><member>tag3</member></tag></entry><entry name='name2' loc='group'><ip-netmask>1.1.1.2</ip-netmask></entry></address></result></response>"
+      allow(RestClient::Request).to receive(:execute).and_return(response)
+      tag_array = @addrequest.get_all_for_tag('tag', 'group')
+      expect{tag_array.length == 0}
+    end
 
-    it 'throws an exception when there was an error from the firewall'
+    it 'returns array for addresses found from each type of address' do
+      response = "<response status='success'><result><address><entry name='name' loc='group'><ip-netmask>1.1.1.1</ip-netmask><tag><member>tag</member></tag></entry><entry name='name2' loc='group'><ip-range>1.1.1.1-1.1.1.9</ip-range><tag><member>tag</member></tag></entry><entry name='name3' loc='group'><fqdn>1.1.1.1-1.1.1.9</fqdn><tag><member>tag</member></tag></entry></address></result></response>"
+      allow(RestClient::Request).to receive(:execute).and_return(response)
+      tag_array = @addrequest.get_all_for_tag('tag', 'group')
+      expect{tag_array.length == 3}
+    end
+
+    it 'throws an exception when there was an error from the firewall' do
+      response = "<response status = 'error' code = '403'><result><msg>Invalid credentials.</msg></result></response>"
+      allow(RestClient::Request).to receive(:execute).and_return(response)
+      expect{@addrequest.get_all_for_tag('tag', 'group')}.to raise_error(Exception)
+    end
+
+    it 'throws an exception making the rest call' do
+      allow(RestClient::Request).to receive(:execute).and_raise(Exception)
+      expect{@addrequest.get_all_for_tag('tag', 'group')}.to raise_error(Exception)
+    end
 
   end
 
